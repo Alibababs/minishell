3bb6a45e8572984cc883a99841faace74103ede2
Alix <alixbailly23@gmail.com>
2024-12-09 17:20:05 +0100

    * Look if '0' is in_single_quotes at the beginning

--- srcs/expander/exit_status.c
+++ srcs/expander/exit_status.c
@@ -6,7 +6,7 @@
 /*   By: alibabab <alibabab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 16:45:03 by alibabab          #+#    #+#             */
-/*   Updated: 2024/12/09 16:57:24 by alibabab         ###   ########.fr       */
+/*   Updated: 2024/12/09 17:19:21 by alibabab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -35,16 +35,14 @@ static int	get_new_len(char *value, int status_len)
 	return (new_len);
 }
 
-static void	process_exit_status(char *value, char *status_str, char *new_value,
-		char *ptr)
+static void	process_exit_status(char *status_str, char *new_value, char *ptr)
 {
 	int	i;
 
 	i = 0;
 	while (ptr[i])
 	{
-		if (ptr[i] == '$' && ptr[i + 1] == '?' && !in_single_quotes(value,
-				&ptr[i]))
+		if (ptr[i] == '$' && ptr[i + 1] == '?')
 		{
 			ft_strcpy(new_value, status_str);
 			new_value += ft_strlen(status_str);
@@ -65,12 +63,21 @@ char	*handle_exit_status(char *value, t_token **h_token)
 	char	*status_str;
 	char	*new_value;
 	char	*ptr;
+	int		i;
 
+	i = 0;
+	while (value[i])
+	{
+		if (value[i] == '$' && value[i + 1] == '?' && in_single_quotes(value,
+				&value[i]))
+			return (value);
+		i++;
+	}
 	status_str = ft_itoa(g_exit_status);
 	new_value = malloc(get_new_len(value, ft_strlen(status_str)) + 1);
 	if (!new_value)
 		mem_error_tokens(h_token);
 	ptr = value;
-	process_exit_status(value, status_str, new_value, ptr);
+	process_exit_status(status_str, new_value, ptr);
 	return (new_value);
 }
