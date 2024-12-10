9869247d2a067eb79c383df46a7019862416abc8
Alix <alixbailly23@gmail.com>
2024-12-10 02:12:53 +0100

    * Nothing

--- srcs/main/free.c
+++ srcs/main/free.c
@@ -6,7 +6,7 @@
 /*   By: alibabab <alibabab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 12:02:05 by p0ulp1            #+#    #+#             */
-/*   Updated: 2024/12/10 01:58:41 by alibabab         ###   ########.fr       */
+/*   Updated: 2024/12/10 02:12:15 by alibabab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,14 +17,6 @@ void	ft_error(t_data **data, char *msg)
 	printf("An error occured:\n");
 	printf("%s\n", msg);
 	(void)data;
-}
-
-void	mem_error_env(t_env **h_env)
-{
-	printf("A memory allocation has failed.\n");
-	printf("Please provide more memory to the process.\nExiting\n");
-	exit(1);
-	(void)h_env;
 }
 
 void	mem_error(t_data **data)
@@ -74,6 +66,9 @@ void	free_tokens(t_data *data)
 
 void	free_data(t_data **data)
 {
+	int	i;
+
+	i = 0;
 	if (!data || !*data)
 		return ;
 	if ((*data)->h_tokens)
