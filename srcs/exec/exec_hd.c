e7c7ae42cfdcce5bf0d5cd817af2cc6b4a9ceeb4
Alix <alixbailly23@gmail.com>
2025-01-21 19:10:31 +0100

    * Ctrl+C is going to kill me

--- srcs/exec/exec_hd.c
+++ srcs/exec/exec_hd.c
@@ -6,13 +6,13 @@
 /*   By: alibabab <alibabab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 14:43:26 by phautena          #+#    #+#             */
-/*   Updated: 2025/01/21 18:44:04 by alibabab         ###   ########.fr       */
+/*   Updated: 2025/01/21 19:10:07 by alibabab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static int	here_doc_write(t_token *tp, int fd_in)
+static int	here_doc_write(t_token *tp, int fd_in, t_data **data)
 {
 	char	*line;
 
@@ -26,6 +26,11 @@ static int	here_doc_write(t_token *tp, int fd_in)
 			ft_putstr_fd(tp->next->value, 2);
 			ft_putstr_fd("')\n", 2);
 			break ;
+		}
+		if (tp->next->was_quote == false)
+		{
+			line = handle_exit_status(line, data);
+			line = handle_dollar(line, data);
 		}
 		if (ft_strcmp(line, tp->next->value) == 0)
 		{
@@ -43,11 +48,10 @@ int	init_here_doc(t_token *tp, t_data **data)
 {
 	int	infile;
 
-	(void)data;
 	infile = open("temp.txt", O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (infile < 0)
 		return (-1);
-	here_doc_write(tp, infile);
+	here_doc_write(tp, infile, data);
 	if (infile > 0)
 		close(infile);
 	infile = open("temp.txt", O_RDONLY);
