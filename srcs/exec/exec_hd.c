3d7a2c58fe7a49704059994e5e084db4c72e076b
Alix <alixbailly23@gmail.com>
2025-01-21 18:44:44 +0100

    * Ctrl+D work perfectly but need to implement Ctrl+C

--- srcs/exec/exec_hd.c
+++ srcs/exec/exec_hd.c
@@ -6,54 +6,50 @@
 /*   By: alibabab <alibabab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/21 14:43:26 by phautena          #+#    #+#             */
-/*   Updated: 2025/01/21 17:27:22 by alibabab         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:44:04 by alibabab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
-static void	here_doc_write(t_token *tp, int fd_in, t_data **data)
+static int	here_doc_write(t_token *tp, int fd_in)
 {
 	char	*line;
 
-	ft_signals(3);
 	while (1)
 	{
-		ft_putstr_fd("> ", STDOUT_FILENO);
-		line = get_next_line(0);
-		if (!line || g_exit_status == 130)
+		line = readline("> ");
+		if (!line)
+		{
+			ft_putstr_fd("minishell: warning: here-document delimited by end-of-file (wanted `",
+				2);
+			ft_putstr_fd(tp->next->value, 2);
+			ft_putstr_fd("')\n", 2);
+			break ;
+		}
+		if (ft_strcmp(line, tp->next->value) == 0)
 		{
 			free(line);
 			break ;
 		}
-		if (tp->next->was_quote == false)
-		{
-			line = handle_exit_status(line, data);
-			line = handle_dollar(line, data);
-		}
-		if (!ft_strncmp(line, tp->next->value, ft_strlen(tp->next->value)))
-			return (free(line));
-		ft_putstr_fd(line, fd_in);
+		write(fd_in, line, ft_strlen(line));
+		write(fd_in, "\n", 1);
 		free(line);
 	}
-	ft_signals(1);
+	return (0);
 }
 
 int	init_here_doc(t_token *tp, t_data **data)
 {
 	int	infile;
 
+	(void)data;
 	infile = open("temp.txt", O_CREAT | O_RDWR | O_TRUNC, 0644);
 	if (infile < 0)
 		return (-1);
-	here_doc_write(tp, infile, data);
+	here_doc_write(tp, infile);
 	if (infile > 0)
 		close(infile);
-	if (g_exit_status == 130)
-	{
-		unlink("temp.txt");
-		return (-1);
-	}
 	infile = open("temp.txt", O_RDONLY);
 	return (infile);
 }
--- srcs/exec/exec_redirs.c
+++ srcs/exec/exec_redirs.c
@@ -6,7 +6,7 @@
 /*   By: alibabab <alibabab@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/16 10:17:55 by phautena          #+#    #+#             */
-/*   Updated: 2025/01/21 17:14:52 by alibabab         ###   ########.fr       */
+/*   Updated: 2025/01/21 18:43:39 by alibabab         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,7 +39,7 @@ int	init_infiles(t_token *tp, t_cmd *cmd, t_data **data)
 				cmd->infile = init_here_doc(tp, data);
 			else
 				cmd->infile = open(tp->next->value, O_RDONLY);
-			if (cmd->infile == -1 && g_exit_status != 130)
+			if (cmd->infile == -1)
 			{
 				perror(tp->next->value);
 				g_exit_status = 1;
